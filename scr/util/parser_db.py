995ba52262c51f13c5d3c79b49bd6bdf6b01e6b8
tulio116 <lovetul@gmail.com>
2020-09-29 11:15:33 +0700

    Fixed insert query

--- scr/util/parser_db.py
+++ scr/util/parser_db.py
@@ -3,11 +3,10 @@ import json
 import cx_Oracle
 import datetime
 import time
-import logging
+import log
 
 from environment import *
 
-logger = logging.getLogger(__name__)
 STATUS_OPEN = "processing"
 STATUS_CLOSE = "done"
 
@@ -16,27 +15,26 @@ def update_status(vendor, parser, state):
 	connection = cx_Oracle.connect(ORACLE_USERNAME, ORACLE_PASSWORD, dsn_tns, encoding="UTF-8",nencoding="UTF-8")
 	cursor = None
 	try:	
-		ts = datetime.datetime.now()	
+		# ts = datetime.datetime.now()	
 		query = f"""MERGE INTO PARSER_STATUS d
 		USING (SELECT * from PARSER_STATUS WHERE VENDOR = '{vendor}' AND PARSER = '{parser}' AND trunc(DT,'DD') = trunc(sysdate, 'DD')) s
 		ON (d.vendor = s.vendor AND d.parser = s.parser AND d.dt = s.dt)
 		WHEN MATCHED THEN UPDATE SET d.status = '{state}'
-		WHEN NOT MATCHED THEN INSERT (vendor, parser, status, dt) VALUES ('{vendor}', '{parser}', '{state}', :t_val);
+		WHEN NOT MATCHED THEN INSERT (vendor, parser, status, dt) VALUES ('{vendor}', '{parser}', '{state}', sysdate);
 		"""
 		# query = f"INSERT INTO PARSER_STATUS VALUES ('{vendor}','{parser}','{state}', {today}"
 		cursor = connection.cursor()
 		execute_time = time.time()	
-		cursor.prepare(query)	
-		print(query)
-		cursor.setinputsizes(t_val=cx_Oracle.TIMESTAMP)
-		cursor.execute(None, {'t_val': ts})
+		# cursor.prepare(query)	
+		# print(query)
+		# cursor.setinputsizes(t_val=cx_Oracle.TIMESTAMP)
+		cursor.execute(query)
 		connection.commit()
 		elapsed_time = time.time() - execute_time		
-		logger.info(f'parser.update_status.done: elapsed time = {elapsed_time:.2f} sec')
+		log.i(f'parser.update_status.done: elapsed time = {elapsed_time:.2f} sec')
 
 	except Exception as e:		
-		logger.exception(f'parser.update_status.err: {str(e)}')
-		print(e)
+		log.e(f'parser.update_status.err: {str(e)}')
 	finally:
 		if cursor is not None:
 			cursor.close()
