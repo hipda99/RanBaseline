25457af216fb93176ef31003b08b6011c24ae838
tulio116 <lovetul@gmail.com>
2020-09-29 11:10:58 +0700

    Update

--- scr/util/parser_db.py
+++ scr/util/parser_db.py
@@ -15,17 +15,20 @@ def update_status(vendor, parser, state):
 	dsn_tns = cx_Oracle.makedsn(ORACLE_HOST, ORACLE_PORT, ORACLE_SID)
 	connection = cx_Oracle.connect(ORACLE_USERNAME, ORACLE_PASSWORD, dsn_tns, encoding="UTF-8",nencoding="UTF-8")
 	cursor = None
-	try:		
+	try:	
+		ts = datetime.datetime.now()	
 		query = f"""MERGE INTO PARSER_STATUS d
 		USING (SELECT * from PARSER_STATUS WHERE VENDOR = '{vendor}' AND PARSER = '{parser}' AND trunc(DT,'DD') = trunc(sysdate, 'DD')) s
 		ON (d.vendor = s.vendor AND d.parser = s.parser AND d.dt = s.dt)
 		WHEN MATCHED THEN UPDATE SET d.status = '{state}'
-		WHEN NOT MATCHED THEN INSERT (vendor, parser, status, dt) VALUES ('{vendor}', '{parser}', '{state}', {datetime.datetime.now()});
+		WHEN NOT MATCHED THEN INSERT (vendor, parser, status, dt) VALUES ('{vendor}', '{parser}', '{state}', :t_val);
 		"""
 		# query = f"INSERT INTO PARSER_STATUS VALUES ('{vendor}','{parser}','{state}', {today}"
 		cursor = connection.cursor()
-		execute_time = time.time()		
-		cursor.execute(query)
+		execute_time = time.time()	
+		cursor.prepare(query)	
+		cursor.setinputsizes(t_val=cx_Oracle.TIMESTAMP)
+		cursor.execute(None, {'t_val': ts})
 		connection.commit()
 		elapsed_time = time.time() - execute_time		
 		logger.info(f'parser.update_status.done: elapsed time = {elapsed_time:.2f} sec')
