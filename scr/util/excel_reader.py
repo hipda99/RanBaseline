464a177b9024e7b753a88069c2d5f02316b7d9e7
tulio116 <lovetul@gmail.com>
2020-09-30 16:00:03 +0700

    Fixed convert excel to String

--- scr/util/excel_reader.py
+++ scr/util/excel_reader.py
@@ -3,5 +3,6 @@ from pandas import ExcelFile
 
 def read_excel_mapping(file_mapping_path_name, sheet_index):
     xls = ExcelFile(file_mapping_path_name)
-    df = xls.parse(xls.sheet_names[sheet_index])
+    ncols = xls.book.sheet_by_index(sheet_index).ncols
+    df = xls.parse(xls.sheet_names[sheet_index], converters={i : str for i in range(ncols)})
     return df
