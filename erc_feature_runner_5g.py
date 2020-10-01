aaaa41878932abe57e7bc45b7d6b4554308d5de2
tulio116 <lovetul@gmail.com>
2020-10-01 17:51:03 +0700

    Added Ericsson feature runner

---
+++ erc_feature_runner_5g.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import datetime
+
+import log
+from environment import ERICSSON_FEATURE_VENDOR
+from scr.parser import main_baseline_parser
+from scr.util import parser_db
+
+TIME_START_SCRIPT = datetime.datetime.now()
+log.i(" ", ERICSSON_FEATURE_VENDOR, "5G")
+log.i(" ", ERICSSON_FEATURE_VENDOR, "5G")
+log.i("           ", ERICSSON_FEATURE_VENDOR)
+log.i("Start Script : " + ERICSSON_FEATURE_VENDOR, ERICSSON_FEATURE_VENDOR, "5G")
+
+parser_db.update_status(ERICSSON_FEATURE_VENDOR, '5G', parser_db.STATUS_OPEN)
+
+main_baseline_parser.run(ERICSSON_FEATURE_VENDOR, "5G")
+
+TIME_END_SCRIPT = datetime.datetime.now()
+log.i("Time : " + str(TIME_END_SCRIPT - TIME_START_SCRIPT), ERICSSON_FEATURE_VENDOR, "5G")
+
+log.i("--------------------------------", ERICSSON_FEATURE_VENDOR, "5G")
+log.i("--------------------------------", ERICSSON_FEATURE_VENDOR, "5G")
+log.count()
+log.i("--------------------------------", ERICSSON_FEATURE_VENDOR, "5G")
+log.i("--------------------------------", ERICSSON_FEATURE_VENDOR, "5G")
+
+log.i("Done all : " + ERICSSON_FEATURE_VENDOR + " 5G", ERICSSON_FEATURE_VENDOR, "5G")
+
+parser_db.update_status(ERICSSON_FEATURE_VENDOR, '5G', parser_db.STATUS_CLOSE)
+
+log.i("           ", ERICSSON_FEATURE_VENDOR)
+log.i("           ", ERICSSON_FEATURE_VENDOR)
+log.i("           ", ERICSSON_FEATURE_VENDOR)
+log.i("           ", ERICSSON_FEATURE_VENDOR)
+log.i("           ", ERICSSON_FEATURE_VENDOR)
+log.i("           ", ERICSSON_FEATURE_VENDOR)
