8e4763328e73c8bcee0c45135d5b5b35d1109b3e
tulio116 <lovetul@gmail.com>
2020-10-22 16:10:36 +0700

    Test

--- scr/config/parser/data_source_parser_debug.py
+++ scr/config/parser/data_source_parser_debug.py
@@ -44,6 +44,7 @@ def read_data_source(vendor):
             continue
 
         # For test only 
+        print(row['Path'])
         if 'STH-ENM' not in row['Path'] or 'ZTE_UME' not in row['Path']:
             continue
 
