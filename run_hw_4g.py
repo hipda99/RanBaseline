04579adfcca69836d3b2a9dfd3e51114a6babc3a
tulio116 <lovetul@gmail.com>
2020-10-27 16:50:34 +0700

    Update test

--- run_hw_4g.py
+++ run_hw_4g.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 import log
-from environment import ZTE_VENDOR, ERICSSON_VENDOR, HUAWEI_VENDOR
+from environment import ZTE_VENDOR, ERICSSON_VENDOR, HUAWEI_VENDOR, ERICSSON_FEATURE_VENDOR
 from scr.parser import main_baseline_parser
 
 #
 # main_baseline_parser.run_baseline(ERICSSON_VENDOR, "2G")
 # main_baseline_parser.run_baseline(ERICSSON_VENDOR, "3G")
 # main_baseline_parser.run_baseline(ERICSSON_VENDOR, "4G")
+main_baseline_parser.run_baseline(ERICSSON_FEATURE_VENDOR, "4G")
 #
 # main_baseline_parser.run_baseline(ZTE_VENDOR, "2G")
 # main_baseline_parser.run_baseline(ZTE_VENDOR, "3G")
@@ -15,5 +16,5 @@ from scr.parser import main_baseline_parser
 #
 # main_baseline_parser.run_baseline(HUAWEI_VENDOR, "2G")
 # main_baseline_parser.run_baseline(HUAWEI_VENDOR, "3G")
-main_baseline_parser.run_baseline(HUAWEI_VENDOR, "4G")
+# main_baseline_parser.run_baseline(HUAWEI_VENDOR, "4G")
 log.i("Update Baseline HW 4G ::: Done ")
