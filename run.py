35b896fbc715a14e12262e8bac43fad665b77ce4
tulio116 <lovetul@gmail.com>
2020-10-27 16:45:12 +0700

    Test

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from environment import HUAWEI_VENDOR, ERICSSON_VENDOR, ZTE_VENDOR
+from environment import HUAWEI_VENDOR, ERICSSON_VENDOR, ZTE_VENDOR, ERICSSON_FEATURE_VENDOR
 from scr.parser import main_baseline_parser
 from scr.parser.zte import zte_baseline_parser, zte_filler
 import sys
@@ -10,11 +10,12 @@ import log
 # main_baseline_parser.run_baseline(ERICSSON_VENDOR, "2G")
 # main_baseline_parser.run_baseline(ERICSSON_VENDOR, "3G")
 # main_baseline_parser.run_baseline(ERICSSON_VENDOR, "4G")
+main_baseline_parser.run(ERICSSON_FEATURE_VENDOR, "4G")
 
 # main_baseline_parser.run_baseline(ZTE_VENDOR, "2G")
 # main_baseline_parser.run_baseline(ZTE_VENDOR, "3G")
-main_baseline_parser.run_baseline(ZTE_VENDOR, "4G")
-main_baseline_parser.run_baseline(ZTE_VENDOR, "5G")
+# main_baseline_parser.run_baseline(ZTE_VENDOR, "4G")
+# main_baseline_parser.run_baseline(ZTE_VENDOR, "5G")
 
 # main_baseline_parser.run_baseline(HUAWEI_VENDOR, "2G")
 # main_baseline_parser.run_baseline(HUAWEI_VENDOR, "3G")
