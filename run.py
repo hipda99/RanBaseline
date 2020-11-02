1045824ac2818434f0834a4186cc1611a1c9f0e8
tulio116 <lovetul@gmail.com>
2020-11-02 12:01:59 +0700

    Update run

--- run.py
+++ run.py
@@ -9,14 +9,14 @@ import log
 
 # main_baseline_parser.run_baseline(ERICSSON_VENDOR, "2G")
 # main_baseline_parser.run_baseline(ERICSSON_VENDOR, "3G")
-main_baseline_parser.run_baseline(ERICSSON_VENDOR, "4G")
-main_baseline_parser.run_baseline(ERICSSON_VENDOR, "5G")
+# main_baseline_parser.run_baseline(ERICSSON_VENDOR, "4G")
+# main_baseline_parser.run_baseline(ERICSSON_VENDOR, "5G")
 # main_baseline_parser.run(ERICSSON_FEATURE_VENDOR, "4G")
 
 # main_baseline_parser.run_baseline(ZTE_VENDOR, "2G")
 # main_baseline_parser.run_baseline(ZTE_VENDOR, "3G")
-# main_baseline_parser.run_baseline(ZTE_VENDOR, "4G")
-# main_baseline_parser.run_baseline(ZTE_VENDOR, "5G")
+main_baseline_parser.run_baseline(ZTE_VENDOR, "4G")
+main_baseline_parser.run_baseline(ZTE_VENDOR, "5G")
 
 # main_baseline_parser.run_baseline(HUAWEI_VENDOR, "2G")
 # main_baseline_parser.run_baseline(HUAWEI_VENDOR, "3G")
