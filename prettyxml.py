e17d3225a6f43c4a08bb39694af86b0c0d802074
tulio116 <lovetul@gmail.com>
2021-06-20 10:18:03 +0700

    Debug

---
+++ prettyxml.py
@@ -0,0 +1,5 @@
+import lxml.etree as etree
+
+x = etree.parse("filename")
+print(etree.tostring(x, pretty_print=True))
+
--- scr/parser/zte/zte_baseline_parser.py
+++ scr/parser/zte/zte_baseline_parser.py
@@ -1328,11 +1328,12 @@ def parse_4g(raw_file, frequency_type, field_mapping_dic, cell_level_dic):
 								# 	print(base_xml.nsmap)
 								# 	for node in enb_mo.xpath(f"//*[local-name() = '{test}']"):
 								# 		print(node)
-								# mo_group_collection = enb_mo.xpath(xpath,
-								# 								namespaces={ZTE_XML_DESCRIPTOR: ZTE_XML_DESCRIPTOR_REF,
-								# 											ZTE_XML_DESCRIPTOR_REF_EN: ZTE_XML_DESCRIPTOR_EN})
-								xpath = f"//*[local-name() = 'vsData{parameter_group}']"
+								mo_group_collection = enb_mo.xpath(xpath,
+																namespaces={ZTE_XML_DESCRIPTOR: ZTE_XML_DESCRIPTOR_REF,
+																			ZTE_XML_DESCRIPTOR_REF_EN: ZTE_XML_DESCRIPTOR_EN})
+								# xpath = f"//*[local-name() = 'vsData{parameter_group}']"
 								mo_group_collection = enb_mo.xpath(xpath)
+								log.i(f'Size for mo collection of {parameter_group} = {len(mo_group_collection)}')
 
 								extra_value = ""
 								for enb_moo_ in mo_group_collection:
